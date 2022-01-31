92c4c4e2dcf9c3607ee298c9d7706ba5d9c860dd
ASUS <mehrandab78@gmail.com>
2022-01-31 21:32:25 +0330

    writing ProductRepositoryImpl.java

--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -5,4 +5,36 @@ public class Product extends BaseEntity {
     private int price;
     private Category category;
 
+    public Product() {
+    }
+
+    public Product(String name, int price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- src/main/java/repository/jdbc/ProductRepositoryImpl.java
+++ src/main/java/repository/jdbc/ProductRepositoryImpl.java
@@ -1,34 +1,119 @@
 package repository.jdbc;
 
+import connection.PostgresConnection;
+import entity.Admin;
+import entity.Category;
 import entity.Product;
 import repository.ProductRepository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRepositoryImpl implements ProductRepository {
-
+    Connection connection= PostgresConnection.getInstance().getConnection();
     @Override
     public List<Product> findAll() {
+        String sql="SELECT * from products ";
+        List<Product> products=new ArrayList<>();
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            Product product;
+            ResultSet resultSet=preparedStatement.executeQuery();
+            while(resultSet.next()){
+                Category parentcategory=new Category();
+               parentcategory.setName(  resultSet.getString("category"));
+                product= new Product(resultSet.getString("label"),resultSet.getInt("price"),
+                     parentcategory );
+                products.add(product);
+            }
+            return products;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Product findById(Integer id) {
+        String sql="SELECT * from products ";
+
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            Product product;
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                Category parentcategory=new Category();
+                parentcategory.setName(  resultSet.getString("category"));
+                product= new Product(resultSet.getString("label"),resultSet.getInt("price"),
+                        parentcategory );
+                return product;
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Integer save(Product entity) {
+        String sql="insert into product(label,price,category) values (?,?,?)";
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,entity.getName());
+            preparedStatement.setInt(2,entity.getPrice());
+            preparedStatement.setString(3,entity.getCategory().getName());
+            preparedStatement.executeUpdate();
+            String sql1="select id from product where label=? and price=? andcategory=?";
+            preparedStatement=connection.prepareStatement(sql1);
+            preparedStatement.setString(1,entity.getName());
+            preparedStatement.setInt(2,entity.getPrice());
+            preparedStatement.setString(3,entity.getCategory().getName());
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return  resultSet.getInt("id");
+            }return 0;
+        }catch (SQLException e){
+
+        }
         return null;
     }
 
     @Override
     public void update(Product entity) {
+        String sql="select * from product where  id=?";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setInt(1,entity.getId());
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                String sql1="update product set label=? ,price=?,parentcategory=? where id=?";
+                PreparedStatement preparedStatement1=connection.prepareStatement(sql1);
+                preparedStatement1.setString(1,entity.getName());
+                preparedStatement1.setInt(2,entity.getPrice());
+                preparedStatement1.setString(3,entity.getCategory().getName());
+                preparedStatement1.setInt(4,resultSet.getInt("id"));
+                preparedStatement1.executeUpdate();
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void deleteById(Integer id) {
+        String sql="delete from product where id=?";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setInt(1,id);
+            preparedStatement.executeUpdate();
+        }catch (SQLException e){}
 
     }
 }
