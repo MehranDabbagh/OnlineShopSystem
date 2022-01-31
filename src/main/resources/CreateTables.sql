fcbebcc80cd030e422b4f6665a016e9132924459
ASUS <mehrandab78@gmail.com>
2022-01-31 15:45:13 +0330

    building tables

---
+++ src/main/resources/CreateTables.sql
@@ -0,0 +1,8 @@
+create table if not exists admins(id serial primary key ,username varchar(50),password varchar (50));
+create table if not exists customer(id serial primary key ,firstname varchar (50),lastname varchar(50),
+    username varchar(50),password varchar (50),nationalcode BIGINT ,email varchar (50),address varchar (50),phonenumber BIGINT);
+create table if not exists category(id serial primary key,label varchar (50),parentcategory varchar (50));
+create table if not exists products (id serial primary key,label varchar (50),price int ,category varchar(50));
+create table if not exists cart(id serial primary key, address varchar (50),phonenumber BIGINT,done boolean,customerId integer,
+    CONSTRAINT fk_customer
+    FOREIGN KEY (customerId) REFERENCES customer(id));
